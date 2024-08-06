c079d6d790ad58d4c3797f41fc4170cdfb5d4128
ashfortune <yjb1521@naver.com>
2024-08-06 14:33:00 +0900

    0806

--- src/main/java/com/newlecture/web/controller/DispatcherServlet.java
+++ src/main/java/com/newlecture/web/controller/DispatcherServlet.java
@@ -1,10 +1,12 @@
 package com.newlecture.web.controller;
 
 import java.io.IOException;
+import java.util.Map;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
 import jakarta.servlet.ServletConfig;
@@ -14,26 +16,29 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-@WebServlet(urlPatterns = "/" ,loadOnStartup = 1,initParams = {
-	@WebInitParam(name="path",value ="/WEB-INF/app.properties"),
-	@WebInitParam(name="spring",value ="/WEB-INF/spring-dispatcher.xml")
-})
-
+//@WebServlet(urlPatterns = "/" ,loadOnStartup = 1,initParams = {
+//	@WebInitParam(name="path",value ="/WEB-INF/app.properties"),
+//	@WebInitParam(name="spring",value ="/WEB-INF/spring-dispatcher.xml")
+//})
 public class DispatcherServlet extends HttpServlet{
 	
 //	private Properties prop = new Properties();
-	ApplicationContext app;
+	private ApplicationContext app;
 	
+	public DispatcherServlet(XmlWebApplicationContext context) {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		String path = config.getInitParameter("spring");
 		String realPath = config.getServletContext().getRealPath(path);
-//		app = new ClassPathXmlApplicationContext(realPath);
-		app = new ClassPathXmlApplicationContext(realPath);
+//		app = new ClassPathXmlApplicationContext(path);
+		app = new FileSystemXmlApplicationContext(realPath);
 
 		
 //		try {
-//			String realPath = config.getServletContext().getRealPath(path); 
+//			String realPath = config.getServletContext().getRealPath(path);
 ////			System.out.println(realPath);
 //			InputStream is = new FileInputStream(realPath);
 //			prop.load(is);
@@ -51,24 +56,23 @@ public class DispatcherServlet extends HttpServlet{
 		
 	}
 	
-	
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String uri = req.getRequestURI();
-		
-		Controller controller = (Controller) app.getBean(uri);
-		
-		if(controller == null) {
+		try {
+			Controller controller = (Controller) app.getBean(uri);
+			ModelAndView mv = controller.handleRequest(req, resp);
+			
+			for(Map.Entry<String, Object> en : mv.getModel().entrySet())
+				req.setAttribute(en.getKey(),en.getValue());
+			
+			req.getRequestDispatcher(mv.getViewName()).forward(req, resp);
+		}
+		catch(Exception e){
 			resp.sendError(HttpServletResponse.SC_NOT_FOUND);
 			return;
 		}
 		
-		try {
-			controller.handleRequest(req, resp);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
 //		String className = prop.getProperty(uri);
 //		if (className ==null)
---
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	${test} ${test1} ${test2} ${test3} ${test4} ${test5}
+</body>
+</html>
